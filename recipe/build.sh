25dce25cea73633d6c070b1b53081e384bb32e61
Paul Demorest <pdemores@nrao.edu>
2019-10-18 23:17:23 -0600

    Initial adding stuff

---
+++ recipe/build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+./bootstrap
+./configure --prefix=$PREFIX --disable-local --enable-debug --enable-shared \
+  --includedir=$PREFIX/include/psrchive --with-Qt-dir=no \
+  CXXFLAGS="-O2 -std=c++98" PGPLOT_DIR=$PREFIX/include/pgplot
+make -j4
+make install
+
+# This foo will make conda automatically define a PSRCHIVE env variable
+# when the environment is activated.
+etcdir=$PREFIX/etc/conda
+mkdir -p $etcdir/activate.d
+echo "setenv PSRCHIVE $PREFIX" > $etcdir/activate.d/psrchive-env.csh
+echo "export PSRCHIVE=$PREFIX" > $etcdir/activate.d/psrchive-env.sh
+mkdir -p $etcdir/deactivate.d
+echo "unsetenv PSRCHIVE" > $etcdir/deactivate.d/psrchive-env.csh
+echo "unset PSRCHIVE" > $etcdir/deactivate.d/psrchive-env.sh
